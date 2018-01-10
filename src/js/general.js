afa8cb99ad809cd03870b68793737620465f7961
Frederico <fred.webfront@gmail.com>
2018-01-10 06:55:39 -0200

    Avanco na personalizacao da loja

--- src/js/general.js
+++ src/js/general.js
@@ -24,13 +24,18 @@ $j(window)
         }
     });
 
+
 $j(document)
     .ready(function () {
 
-        let $myCart          = $j('.mycart:not(.mycart--empty)'),
-            $menuCenter      = $j('.menu-lane__center'),
-            $headerContainer = $j('.header-container'),
-            $searchInput     = $j('.search__input');
+        let $myCart           = $j('.mycart:not(.mycart--empty)'),
+            $menuCenter       = $j('.menu-lane__center'),
+            $headerContainer  = $j('.header-container'),
+            $searchInput      = $j('.search__input'),
+            $jointsalesRow    = $j('.jointsales__row'),
+            $seeMore          = $j('.see-more'),
+            $priceClose       = $j('.price-floater__close');
+
 
         if($myCart.length > 0) {
 
@@ -64,6 +69,40 @@ $j(document)
             });
         }
 
+        if($jointsalesRow.length > 0) {
+            let $row;
+
+            $j.each($jointsalesRow, function() {
+                $j(this).append('<div class="jointsales__values"></div>');
+            });
+
+            $j.each($j('.jointsales__values'), function() {
+                $row = $j(this).parents('.jointsales__row');
+
+                $row.find('.comprando-junto').after($row.find('.jointsales__payments'));
+                $j(this).append($row.find('.jointsales__totals'));
+                $j(this).append($row.find('.jointsales__action'));
+            });
+        }
+
+        if($seeMore.length > 0) {
+            $seeMore.on('click', function(){
+                console.info("here?0");
+                $j('html, body').animate({scrollTop: $j('.product__description').offset().top - 100}, 500);
+            });
+        }
+
+
+        if($priceClose.length > 0) {
+            $priceClose.on('click', function(){
+                if($j(this).parents('.price-floater').hasClass('price-floater--closed')) {
+                    $j(this).parents('.price-floater').removeClass('price-floater--closed')
+                } else {
+                    $j(this).parents('.price-floater').addClass('price-floater--closed')
+                }
+
+            });
+        }
 
     })
     .on('resizeStop', function(e){
@@ -71,9 +110,17 @@ $j(document)
         // Dispara após o último movimento de resize parar no navegador.
     })
     .on('scrollStop', function(e){
-        // Safe window.scroll
-        // Dispara após o último movimento de scroll parar no navegador.
+        if($j('.price-floater').length > 0) {
+
+            if($j(window).scrollTop() > ($j('.product-essential').offset().top + ($j('.product-essential').height() / 2))) {
+                $j('.price-floater').addClass('price-floater--on');
+            } else {
+                $j('.price-floater').removeClass('price-floater--on');
+            }
+
+        }
     })
+
     .on('ajaxComplete', function(resp){
         // Safe ajax completed
         // Dispara após completar com sucesso qualquer requisição Ajax, e trás a resposta do Ajax.
